2d9f88e938441cf3fb90f9bcda083b0d43260e27
Mohammadreza Lotfi <mlk.prw@melobit.com>
2020-06-16 11:15:38 +0430

    refactore to ts

--- index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  layer1: require("./Layer1"),
-  layer2: require("./Layer2")
-}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { LAYER1 } from "./Layer1";
+import { LAYER2 } from "./Layer2";
+
+const layer1: LAYER1 = new LAYER1();
+const layer2: LAYER2 = new LAYER2();
+export {
+  layer1, layer2
+};
