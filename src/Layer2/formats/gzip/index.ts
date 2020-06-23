a4af8c6ddc6083e96ccdc346b931de50a8993c0f
koalament <koalament@gmail.com>
2020-06-23 19:36:13 +0430

    gzip browser compatible

--- src/Layer2/formats/gzip/index.ts
+++ src/Layer2/formats/gzip/index.ts
@@ -1,5 +1,5 @@
 import { IFormatter } from "../../../../types/formats";
-import { gzip, ungzip } from "node-gzip";
+import { gzip, unzip } from "../../../libs/gzip";
 export class GzipFormatter implements IFormatter {
   public encode(input: string, callback: (err: Error, encoded?: string) => void): void {
     gzip(Buffer.from(input, "utf-8"))
@@ -8,7 +8,7 @@ export class GzipFormatter implements IFormatter {
       }).catch(callback);
   }
   public decode(input: string, callback: (err: Error, decoded?: string) => void): void {
-    ungzip(Buffer.from(input, "base64")).then((decompressed: Buffer) => {
+    unzip(Buffer.from(input, "base64")).then((decompressed: Buffer) => {
       callback(undefined, decompressed.toString("utf8"));
     }).catch(callback);
   }
---
+++ src/libs/gzip/index.d.ts
@@ -0,0 +1,2 @@
+export function gzip(buffer: Buffer): Promise<Buffer>;
+export function unzip(buffer: Buffer): Promise<Buffer>;
